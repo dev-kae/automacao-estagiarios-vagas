1957d7f27d9c36b1a00dd019e1d2528cc8361587
Kae Lucas <dev.kaeteodoro@gmail.com>
2025-08-04 21:50:28 -0300

    Criada model

---
+++ app/model.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import List
+from enum import Enum
+
+
+class Plataform(Enum):
+    GLASSDOOR = 1
+    LINKEDIN = 2
+
+
+@dataclass
+class Vaga:
+    name: str
+    url: str
+    plataform: Plataform
+
+
+class Site:
+    url: str
+    plataform: Plataform
+    vagas: list[Vaga] = []
+
+
+sites = []
